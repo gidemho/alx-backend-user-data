a2eb70a820d67ac8455d47517156345e559c588b
gidemho <gidemho@gmail.com>
2024-09-12 21:35:10 -0700

    db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -28,16 +28,32 @@ class DB:
         """Add a user to the database and return the User object"""
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)  # Add the new user to the session
-        self._session.commit()
-        # Commit the transaction to save to the database
+        self._session.commit()  # Commit the transaction to save to the database
         return new_user  # Return the newly created User object
 
 
     def find_user_by(self, **kwargs) -> User:
-        """Find user by arbitrary keyword arguments."""
-        try:
-            return self._session.query(User).filter_by(**kwargs).first()
-        except NoResultFound:
+    """Find user by arbitrary keyword arguments."""
+     try:
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
             raise NoResultFound
-        except InvalidRequestError:
-            raise InvalidRequestError
+        return user
+    except InvalidRequestError:
+        raise InvalidRequestError
+
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+    """Update user attributes based on given keyword arguments."""
+
+try:
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError(f"Attribute '{key}' does not exist on User model")
+            setattr(user, key, value)
+        self._session.commit()
+    except NoResultFound:
+        raise NoResultFound(f"User with id {user_id} not found")
+    except InvalidRequestError:
+        raise InvalidRequestError
