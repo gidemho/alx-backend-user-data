3bdfad953af8a79b3cfb432503daf2bead4b88ea
gidemho <gidemho@gmail.com>
2024-09-29 03:09:01 +0100

    add user data related submissions

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,36 +1,19 @@
 #!/usr/bin/env python3
+"""The `user` model's module.
 """
-User model definition using SQLAlchemy.
-"""
-
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-# Initialize the declarative base for SQLAlchemy
+
 Base = declarative_base()
 
 
 class User(Base):
-
+    """Represents a record from the `user` table.
     """
-    SQLAlchemy User model for the users table.
-    Attributes:
-        - id: integer, primary key
-        - email: string, non-nullable
-        - hashed_password: string, non-nullable
-        - session_id: string, nullable
-        - reset_token: string, nullable
-    """
-    __tablename__ = 'users'
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
-
-# Create a SQLite in-memory database (for testing purposes)
-if __name__ == "__main__":
-    engine = create_engine('sqlite:///:memory:')
-    Base.metadata.create_all(engine)
